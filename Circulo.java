7583a041a07e4c295df15e21d8bce4e8a1f7bbb2
Neri Figueroa <36055167+NeriFP@users.noreply.github.com>
2023-08-31 18:17:28 -0600

    Add files via upload

--- Circulo.java
+++ Circulo.java
@@ -1,12 +1,25 @@
+/**
+ * Clase que define un Circulo
+ * @author Neri Figueroa Pérez, Isaías Flores Rendón
+ * @version 1.0
+ */
 public class Circulo implements Figura {
+    /**
+     * double el radio del curculo
+     */
     public double radio;
 
+    /**
+     * Método constructor de un circulo
+     * @param radio radio del círculo
+     */
     public Circulo(double radio){
         this.radio=radio;
     }
 
     @Override
     /**
+     * Método para obtener el área del círculo
      * @param double el area del circulo
      */
     public double area() {
@@ -15,12 +28,17 @@ public class Circulo implements Figura {
 
     @Override
     /**
-     * @param double el perimetro del ciruclo
+     * Metodo para obtener el perimetro del circulo
+     * @param double el perimetro del circulo
      */
     public double perimetro() {
       return 2 * 3.1415 * radio;
     }
 
+    /**
+     * Metodo para obtener los lados de un círculo
+     * @return el numero de lados
+     */
     @Override
     public int getLados() {
         return 0;
